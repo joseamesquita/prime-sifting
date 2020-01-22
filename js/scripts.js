6f4b8e8fbbfffcc99f4ad51d3d3d8e2a96ab6ada
Jose Amesquita and Rachel Schieferstein <jose@>
2020-01-22 14:24:22 -0800

    1. updated prime sifting

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,13 @@ $(document).ready(function(){
     var result = [];
     for(var i = 2; i <= input; i++){
       result.push(i);
-      if(i % 2 === 0){
+      if(i % 2 === 0 && i !== 2){
+        result.pop();
+      }
+      else if(i % 3 === 0 && i !== 3){
+        result.pop();
+      }
+      else if(i % 5 === 0 && i !== 5){
         result.pop();
       }
     }
